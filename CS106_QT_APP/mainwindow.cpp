04c4db9662d7d72e6127240c14e84646c8b82fa9
Thierry <270067227@yoobeestudent.ac.nz>
2023-06-15 11:34:32 +1200

    Updated logout function
    logout
    initiates signin mainwindow.cpp
    displays member, staff, guest

--- CS106_QT_APP/mainwindow.cpp
+++ CS106_QT_APP/mainwindow.cpp
@@ -5,36 +5,31 @@
 #include "./H_files/userdata.h"
 #include "./H_files/bookdata.h"
 #include <QScrollArea>
-
-
 #include <QMessageBox>
 
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , signinWindow(new Signin)
 {
-    //set up ui
-    ui->setupUi(this);
+    ui->setupUi(this); // set up ui
 
+    // Sign out any signed in users
+    logout(); //call the logout function
 
     //hide Account Drop Down Menus
     ui->account_dropDown_guestHome->hide();
     ui->Account_StaffHome->hide();
 
-
-    //find stacked widgets
-    stackedWidget = findChild<QStackedWidget*>("stackedWidget");
-
-    //set first Home screen as guest
-    stackedWidget->setCurrentIndex(1);
+    stackedWidget = findChild<QStackedWidget*>("stackedWidget"); // find stacked widgets
+    stackedWidget->setCurrentIndex(1); // set first Home screen as guest
 
     booksDisplay();
 
-    connect(signinWindow, &Signin::userChecked, this, &MainWindow::loginCheck);
+    connect(signinWindow, &Signin::userChecked, this, &MainWindow::loginCheck); //connect signinWindow to loginCheck
 
     QJsonObject adminUser = files.selectObjectByID(files.filePathMemberData,1);
-
     if(adminUser.empty())
     {
         defaultAdminUser();
@@ -49,105 +44,76 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-
-
+// Guest Sign In
 void MainWindow::loginCheck()
 {
 
-    QJsonObject jsonCurrentUser = files.readFromJson(files.filePathCurrentUser);
+    QJsonObject jsonCurrentUser = files.readFromJson(files.filePathCurrentUser); // read current user file
 
+    QJsonObject jsonUserData = files.readFromJson(files.filePathMemberData); // read user data file
+    QJsonArray jsonUserDataArray = jsonUserData.contains("data") ? jsonUserData["data"].toArray() : QJsonArray(); // get array of data from user json file
+    int rowCount = jsonUserDataArray.size(); // get number of rows in user data file
 
-    QJsonObject jsonUserData = files.readFromJson(files.filePathMemberData);
-    QJsonArray jsonUserDataArray = jsonUserData.contains("data") ? jsonUserData["data"].toArray() : QJsonArray();
-
-    int rowCount = jsonUserDataArray.size();
-
-
-
-    if(signinWindow->staffLoggedOn && !jsonCurrentUser.empty())
+    if(signinWindow->staffLoggedOn && !jsonCurrentUser.empty()) // if staff is logged on
     {
-        // admin is logged in
-        stackedWidget->setCurrentIndex(2);
+        stackedWidget->setCurrentIndex(2); // admin is logged in
 
+        QString loggedInUsername; // set logged in username to empty string
 
-        QString loggedInUsername;
-
-        for (int i = 0; i < rowCount; i++)
+        for (int i = 0; i < rowCount; i++) // loop through user data file
         {
-            QJsonObject object = jsonUserDataArray[i].toObject();
+            QJsonObject object = jsonUserDataArray[i].toObject(); // get object at index i
+            QString username = object["username"].toString(); // get username from object
+            QString password = object["password"].toString(); // get password from object
 
-            QString username = object["username"].toString();
-            QString password = object["password"].toString();
-
-            if (username == "Admin" && password == "Admin")
+            if (username == "Admin" && password == "Admin") // if username and password are admin
             {
                 QMessageBox::warning(this, "Change Password", "Please set a new password for the Admin user.");
             }
 
-
-            if (username == signinWindow->getUsername() && password == signinWindow->getPassword())
+            if (username == signinWindow->getUsername() && password == signinWindow->getPassword()) // if username and password are valid
             {
-                loggedInUsername = username;
+                loggedInUsername = username; // set logged in username to username
                 break;
             }
         }
 
-
         currentUser = loggedInUsername;
-
-
-    }else
+    } else
     {
         stackedWidget->setCurrentIndex(0);
     }
-
-
-
 }
 
-void MainWindow::defaultAdminUser()
+void MainWindow::defaultAdminUser() // create default admin user
 {
     QJsonObject jsonNewUserObj;
-
     jsonNewUserObj["username"] = "Admin";
     jsonNewUserObj["password"] = "Admin";
     jsonNewUserObj["isAdmin"] = true;
     jsonNewUserObj["address"] = "";
     jsonNewUserObj["id"] = 1;
     jsonNewUserObj["phoneNumber"] = "";
-    //default values
     jsonNewUserObj["currentBooks"] = "no books";//will be changed with a custom arrary of books
     jsonNewUserObj["over-dueBooks"] = "no over-due";
 
-
     if(files.writeToJson(files.filePathMemberData ,jsonNewUserObj, 1))
     {
+        //qDebug() << "Admin User Created";
     }
 
 }
 
-
-
-
 void MainWindow::onHomeWindowHidden()
 {
-
-
     loginCheck();
     show();
-
-
 }
-
-
-
 
 void MainWindow::on_pushButton_guestSignIn_clicked()
 {
     signinWindow->show();
 }
-
 
 void MainWindow::on_pushButton_StaffUsersEdit_clicked()
 {
@@ -156,7 +122,6 @@ void MainWindow::on_pushButton_StaffUsersEdit_clicked()
     userdata->show();
 }
 
-
 void MainWindow::on_pushButton_StaffBooksEdit_clicked()
 {
     this->hide();
@@ -164,22 +129,27 @@ void MainWindow::on_pushButton_StaffBooksEdit_clicked()
     bookdata->show();
 }
 
-
+// Log Out - Staff - Button
 void MainWindow::on_pushButton_StaffSignOut_clicked()
+{
+    logout();
+    stackedWidget->setCurrentIndex(0);
+    // output which stacked widget is currently active
+    qDebug() << "Current Stacked Widget: " << stackedWidget->currentIndex();
+}
+
+// Log Out - Staff
+void MainWindow::logout()
 {
     QJsonObject currentJsonUserObj = files.readFromJson(files.filePathCurrentUser);
     QJsonArray jsonUserDataArray = currentJsonUserObj.contains("data") ? currentJsonUserObj["data"].toArray() : QJsonArray();
-
-
     int rowCount = jsonUserDataArray.size();
 
     if (!currentJsonUserObj.isEmpty())
     {
-        for (int i = 0; i < rowCount; ++i) {
+        for (int i = 0; i < rowCount; ++i)
+        {
             QJsonObject object = jsonUserDataArray[i].toObject();
-
-            stackedWidget->setCurrentIndex(1);
-
 
             int id = object["id"].toInt();
             QString currentUsername = object["username"].toString();
@@ -188,29 +158,18 @@ void MainWindow::on_pushButton_StaffSignOut_clicked()
 
             QMessageBox::information(this, "Log Out", "User " + currentUsername + " has Logged out");
         }
-
-
-
-
     }
-
 }
-
-
 
 void MainWindow::on_navAccount_guest_clicked(bool checked)//guest Account DropDown menu
 {
-
     isGuestFrameVisible = !isGuestFrameVisible;
-
 
     if(isGuestFrameVisible)
     {
         ui->account_dropDown_guestHome->show();
-        //isGuestFrameVisible = true;
     }else {
         ui->account_dropDown_guestHome->hide();
-        //isGuestFrameVisible = false;
     }
 
 }
@@ -224,10 +183,8 @@ void MainWindow::on_navAccount_Staff_clicked(bool checked)//staff Account DropDo
     if(isStaffFrameVisible)
     {
         ui->Account_StaffHome->show();
-        //isGuestFrameVisible = true;
     }else {
         ui->Account_StaffHome->hide();
-        //isGuestFrameVisible = false;
     }
 }
 
@@ -237,31 +194,23 @@ void MainWindow::booksDisplay()
 
     QJsonObject jsonCategory = files.readFromJson(files.filePathCategory);
     QJsonArray jsonCategoryDataArray = jsonCategory.contains("data") ? jsonCategory["data"].toArray() : QJsonArray();
-
-
     int rowCount = jsonCategoryDataArray.size();
 
     ui->tableWidget_BookDisplay->setRowCount(rowCount);
 
-
     for(int i = 0; i < rowCount; ++i)
     {
         QJsonObject object = jsonCategoryDataArray[i].toObject();
-
-
         QString categoryName = object["categoryName"].toString();
-
         headerLabels << categoryName;
-
     }
 
-
     ui->tableWidget_BookDisplay->setVerticalHeaderLabels(headerLabels);
-
 }
 
 void MainWindow::on_tableWidget_BookDisplay_cellClicked(int row, int column)
 {
-
+    // Handel cell click event
+    qDebug() << "Row: " << row << " Column: " << column;
 }
 
